2079f67d15521867db10a46a66ee334055a99d46
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-09-04 13:04:54 +0300

    add'

--- my_demo_test_file.py
+++ my_demo_test_file.py
@@ -9,14 +9,14 @@
 # def get_key():
 #     return ''.join(choice(choice(ascii_letters) + choice(digits)) for i in range(50) if i % 2)
 
-# from urllib.parse import urlparse
+from urllib.parse import urlparse
 
-# o = urlparse('http://www.cwi.nl:80/%7Eguido/Python.html')
+o = urlparse('/123')
 # google = urlparse('https://www.google.com.ua/search?hl=ru&source=hp&ei=Dx-NW8WZJMeNsgHAkoFo&q=python+\
 # official+site&oq=python+off&gs_l=psy-ab.3.0.0l10.1931.7068.0.8850.12.11.0.0.0.0.111.999.5j5.11.0..3..0\
 # ...1.1.64.psy-ab..1.10.998.0..0i131k1.90.6IUYpTGC9bw')
 
-# print(google.scheme)
+print(o)
 
 # res_map = {}
 
@@ -24,7 +24,7 @@
 #     res_map[get_key()] = google.path
 
 # print(res_map)
-l = [1]
-a, *rest = l
-print(a)
-print(rest)
+# l = [1]
+# a, *rest = l
+# print(a)
+# print(rest)
