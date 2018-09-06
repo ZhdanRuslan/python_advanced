78923775045d1d42b5e7a289b9165e5247631fa3
Ruslan Zhdan <zhd.ruslan@gmail.com>
2018-09-06 09:11:47 +0300

    my demo

--- my_demo_test_file.py
+++ my_demo_test_file.py
@@ -9,14 +9,14 @@
 # def get_key():
 #     return ''.join(choice(choice(ascii_letters) + choice(digits)) for i in range(50) if i % 2)
 
-from urllib.parse import urlparse
+# from urllib.parse import urlparse
 
-o = urlparse('/123')
+# o = urlparse('/123')
 # google = urlparse('https://www.google.com.ua/search?hl=ru&source=hp&ei=Dx-NW8WZJMeNsgHAkoFo&q=python+\
 # official+site&oq=python+off&gs_l=psy-ab.3.0.0l10.1931.7068.0.8850.12.11.0.0.0.0.111.999.5j5.11.0..3..0\
 # ...1.1.64.psy-ab..1.10.998.0..0i131k1.90.6IUYpTGC9bw')
 
-print(o)
+# print(o)
 
 # res_map = {}
 
@@ -28,3 +28,9 @@ print(o)
 # a, *rest = l
 # print(a)
 # print(rest)
+
+import string
+import random
+token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(7))
+
+print(token)
